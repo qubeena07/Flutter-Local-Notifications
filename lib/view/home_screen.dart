c6717276afccc467ffb76487adf99e5390d35d86
Dipika Ranabhat <qubeena7@gmail.com>
2022-08-16 15:21:04 +0545

    notofication completed

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,7 +11,36 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  void _showNotification() {}
+  FlutterLocalNotificationsPlugin? localNotification;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // ignore: prefer_const_constructors
+    var androidInitialize = AndroidInitializationSettings('ic_launcher');
+
+    var iOSIntialize = const IOSInitializationSettings();
+    var initialzationSettings =
+        InitializationSettings(android: androidInitialize, iOS: iOSIntialize);
+    localNotification = FlutterLocalNotificationsPlugin();
+    localNotification!.initialize(initialzationSettings);
+  }
+
+  Future _showNotification() async {
+    log("Notification wohoöooo");
+    var androidDetails = const AndroidNotificationDetails(
+        "Notification", "Work update",
+        importance: Importance.high);
+    var iOSDetails = const IOSNotificationDetails();
+    var generalNotificationDetails =
+        NotificationDetails(android: androidDetails, iOS: iOSDetails);
+    await localNotification?.show(
+        0,
+        "Work Time",
+        "You have a pending work please complete it",
+        generalNotificationDetails);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,6 +62,7 @@ class _HomeScreenState extends State<HomeScreen> {
               style: ElevatedButton.styleFrom(
                   primary: const Color.fromARGB(255, 196, 176, 0)),
               onPressed: _showNotification,
+
               // => NotificationApi.showNotification(
               //     title: "Work Time",
               //     body: "You have a pending work to complete"),
